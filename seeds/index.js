52c78d212e4003ba8faab61cfd334ce18ed6db2b
MianAnswer <miananswer@hotmail.com>
2021-11-23 18:41:04 -0500

    Adding Images

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, magnam ipsa, consequuntur fuga amet sequi quod pariatur a, ab qui ex nisi! Odit beatae provident consectetur? Eos sequi esse qui. Ducimus voluptatum, quisquam deserunt a cumque iusto facere accusantium, velit quae, sapiente voluptate. Alias molestiae, dolor dolores laudantium totam quae aspernatur distinctio sapiente quod qui iure odit recusandae voluptas nostrum!',
+            price
         });
         await camp.save();
     }
