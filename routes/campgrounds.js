56b749b48c870082a1fffab1d027e16c15959023
MianAnswer <miananswer@hotmail.com>
2021-11-30 20:08:08 -0500

    Flash Success Partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,7 +2,6 @@ const express = require('express');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const Review = require('../models/review');
 const { campgroundSchema } = require('../schemas');
 
 const router = express.Router();
@@ -46,12 +45,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndDelete(id, { ...req.body.campground });
+    await Campground.findByIdAndDelete(id, { ...req.body.campground });
+    req.flash('success', 'Successfully deleted campground!')
     res.redirect('/campgrounds');
 }));
 
