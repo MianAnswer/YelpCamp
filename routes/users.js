56eb67bc8a95fd8c917d79fff4e0eda9bc83c78d
MianAnswer <miananswer@hotmail.com>
2021-12-11 22:18:03 -0500

    Restructure Routes

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const router = express.Router();
 const users = require('../controllers/users');
 const passport = require('passport');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout', users.logout)
 
