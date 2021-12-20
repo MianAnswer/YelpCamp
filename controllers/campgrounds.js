10a92d6284810c54e65ab6241f561bed644d5ac7
MianAnswer <miananswer@hotmail.com>
2021-12-19 20:14:22 -0500

    Add Upload to Edit

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -50,6 +50,14 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const arrOfImages = req.files.map(file => {
+        return {
+            url: file.path,
+            filename: file.filename
+        }
+    });
+    campground.images.push(...arrOfImages);
+    await campground.save();
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`);
 }
