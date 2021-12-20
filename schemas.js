3f68ebfa7be753b424f14707d8e96b2d60b758f7
MianAnswer <miananswer@hotmail.com>
2021-12-20 08:53:04 -0500

    Delete Images

--- schemas.js
+++ schemas.js
@@ -10,7 +10,8 @@ module.exports.campgroundSchema = Joi.object({
         price: Joi.number().min(0).required(),
         location: Joi.string().required(),
         description: Joi.string().required()
-    }).required()
+    }).required(),
+    deleteImages: Joi.array()
 }).required();
 
 module.exports.reviewSchema = Joi.object({
